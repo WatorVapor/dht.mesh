e0210124fdbd30bf7d0293c6db4335331a841b50
Wator Vapor <watovapor@gmail.com>
2020-04-13 15:27:30 +0000

    publish & delivery

---
+++ cli/ut.proxy.daemon.js
@@ -0,0 +1,33 @@
+'use strict';
+const path = require('path');
+//console.log(':: __filename=<',__filename,'>');
+const dhtPath = '/storage/dhtfs/cluster/dht_mesh_' + path.parse(__filename).name;
+const config = {
+  listen:{
+    port:19991
+  },
+  entrance:[
+    {
+      host:'ermu4.wator.xyz',
+      port:19990
+    },
+    {
+      host:'ermu3.wator.xyz',
+      port:19990
+    }
+  ],
+  reps: {
+    dht:dhtPath,
+  },
+  concern:false
+};
+//console.log(':: config=<',config,'>');
+const DHT = require('../src/dht.js');
+const dht = new DHT(config);
+//console.log(':: dht=<',dht,'>');
+const peer = dht.peerInfo();
+console.log(':: peer=<',peer,'>');
+
+const DHTDaemon = require('../api/DHTDaemon.js');
+const daemonUTListenChannel = 'dht.mesh.api.daemon.listen.ut';
+const daemon = new DHTDaemon(dht,daemonUTListenChannel);
