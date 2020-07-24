597dfe031aa69692fb253cef219d4ceeacddf282
Wator Vapor <watovapor@gmail.com>
2020-07-24 01:18:55 +0000

    ermu save

---
+++ docker/run.kvstore.daemon.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+PARENT_DiR=$(dirname $SCRIPT_DIR)
+echo "SCRIPT_DIR:=${SCRIPT_DIR}"
+echo "PARENT_DiR:=${PARENT_DiR}"
+docker rm kv.storage
+docker run -d \
+  -v /etc/group:/etc/group:ro \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /storage/dhtfs/cluster:/storage/dhtfs/cluster \
+  -v /dev/shm/:/dev/shm/ \
+  -v ${PARENT_DiR}:${PARENT_DiR} \
+  -u $(id -u $USER):$(id -g $USER) \
+  -w ${PARENT_DiR} \
+  --net host \
+  --name "kv.storage" \
+  node:lts node ./kvstore/StorageKV.js
