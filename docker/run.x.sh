24b8b999277fc76720570d61fc14a9517ea406c1
Wator Vapor <watovapor@gmail.com>
2020-07-24 00:59:07 +0000

    ermu save

--- docker/run.x.sh
+++ docker/run.x.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+echo "SCRIPT_DIR:=${SCRIPT_DIR}"
+docker run -it\
+  -v /etc/group:/etc/group:ro\
+  -v /etc/passwd:/etc/passwd:ro\
+  -v ${SCRIPT_DIR}:${SCRIPT_DIR}\
+  -u $(id -u $USER):$(id -g $USER)\
+  -w ${SCRIPT_DIR}\
+  node:lts /bin/bash
