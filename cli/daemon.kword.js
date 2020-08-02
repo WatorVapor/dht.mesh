07c703c3a7a2bc71efef200207554d4fda0c994c
Wator Vapor <watovapor@gmail.com>
2020-08-02 15:02:12 +0000

    devide to other proccess

---
+++ cli/daemon.kword.js
@@ -0,0 +1,33 @@
+'use strict';
+const path = require('path');
+//console.log(':: __filename=<',__filename,'>');
+const dhtPath = '/storage/dhtfs/cluster/dht_mesh_' + path.parse(__filename).name;
+const config = {
+  listen:{
+    port:19991
+  },
+  entrance:[
+    {
+      host:'ermu4.wator.xyz',
+      port:19990
+    },
+    {
+      host:'ermu3.wator.xyz',
+      port:19990
+    }
+  ],
+  reps: {
+    dht:dhtPath,
+  },
+  trap:true
+};
+//console.log(':: config=<',config,'>');
+const DHT = require('../src/dht.js');
+const dht = new DHT(config);
+//console.log(':: dht=<',dht,'>');
+const peer = dht.peerInfo();
+console.log(':: peer=<',peer,'>');
+
+const daemonKeyWordListenChannel = 'dht.mesh.api.daemon.listen.kword';
+const DHTDaemon = require('../api/DHTDaemon.js');
+const daemon = new DHTDaemon(dht,daemonKeyWordListenChannel);
---
+++ docker/run.dht.keyvalue.daemon.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+PARENT_DiR=$(dirname $SCRIPT_DIR)
+echo "SCRIPT_DIR:=${SCRIPT_DIR}"
+echo "PARENT_DiR:=${PARENT_DiR}"
+DOCKER_MAME=dht.daemon
+docker stop ${DOCKER_MAME}
+docker rm ${DOCKER_MAME}
+docker run -d \
+  -v /etc/group:/etc/group:ro \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /storage/dhtfs/cluster:/storage/dhtfs/cluster \
+  -v /dev/shm/:/dev/shm/ \
+  -v ${PARENT_DiR}:${PARENT_DiR} \
+  -u $(id -u $USER):$(id -g $USER) \
+  -w ${PARENT_DiR} \
+  --net host \
+  --memory=512M \
+  --cpu-shares=256 \
+  --name "${DOCKER_MAME}" \
+  --restart=always \
+  node:lts node ./cli/daemon.js
