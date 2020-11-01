2023dab356818638e006a45c285eba3b40722738
Wator Vapor <watovapor@gmail.com>
2020-11-01 14:15:19 +0000

    kv contents

--- api/KVContent.js
+++ api/KVContent.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const DHTUtils = require("./DHTUtils.js");
 
-module.exports = class KVFolder {
+module.exports = class KVContent {
   constructor(path) {
     this._root = path;
     if (!fs.existsSync(path)) {
@@ -10,11 +10,12 @@ module.exports = class KVFolder {
     }
     this.utils_ = new DHTUtils();
   }
-  put(key,content) {
-    let keyAddress = this.getKeyAddress_(key);
-    //console.log('KVFolder::put: keyAddress=<',keyAddress,'>');
-    let keyPath = path.dirname(keyAddress);
-    //console.log('KVFolder::put: keyPath=<',keyPath,'>');
+  put(content) {
+    const key = this.utils_.calcAddress(content);
+    const keyAddress = this.getKeyAddress_(key);
+    //console.log('KVContent::put: keyAddress=<',keyAddress,'>');
+    const keyPath = path.dirname(keyAddress);
+    //console.log('KVContent::put: keyPath=<',keyPath,'>');
     if (!fs.existsSync(keyPath)) {
       fs.mkdirSync(keyPath,{ recursive: true });
     }
@@ -24,7 +25,7 @@ module.exports = class KVFolder {
   
   get(key) {
     let keyAddress = this.getKeyAddress_(key);
-    //console.log('KVFolder::get: keyAddress=<',keyAddress,'>');
+    //console.log('KVContent::get: keyAddress=<',keyAddress,'>');
     if (fs.existsSync(keyAddress)) {
       let content = fs.readFileSync(keyAddress, 'utf8');
       return content;
@@ -36,11 +37,9 @@ module.exports = class KVFolder {
   getKeyAddress_(key) {
     const keyB32 = this.utils_.calcAddress(key);
     let pathAddress = this._root 
-    pathAddress += '/' + keyB32.substring(0,3);
-    pathAddress += '/' + keyB32.substring(3,6);
-    pathAddress += '/' + keyB32.substring(6,9);
+    pathAddress += '/' + keyB32.substring(0,2);
     pathAddress += '/' + keyB32;
-    //console.log('KVFolder::get: pathAddress=<',pathAddress,'>');
+    //console.log('KVContent::get: pathAddress=<',pathAddress,'>');
     return pathAddress;
   }
 }
